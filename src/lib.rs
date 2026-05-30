09400c9a5261f58bbbb2a224d6200e7b613a8355
msujas <kenny.mrshll@gmail.com>
2026-05-30 16:10:29 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use pyo3::prelude::*;
+
+
+// integration - cbfdir, ponidir, tthmin, tthmax, tthbins, chimin, chimax, chibins, maskfile, pfactor
+
+/// A Python module implemented in Rust.
+#[pymodule]
+mod multipos_rustpy {
+use std::{fs::create_dir, path::Path, time::{Instant}};
+
+use multipos_rust::functions::MultiFile;
+use pyo3::prelude::*;
+
+    /// Formats the sum of two numbers as string.
+    #[pyfunction]
+    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+        Ok((a + b).to_string())
+    }
+
+
+    #[pyfunction]
+    fn integrate_rp(cbfdir:String, ponidir:String, tthmin: f64, tthmax: f64, tthbins: usize, chimin: f64, chimax:f64,
+                    chibins: usize, pfactor: f64, maskfile:Option<String>, savecakes:bool, outsubdir:String, cakemaskfile:Option<String>){
+        let timestamp = Instant::now();
+        let tmp:String;
+        let mask = match maskfile{
+            None => None,
+            Some(s) => {tmp = s;
+                Some(Path::new(&tmp))},
+        };
+        let mut cakedir = String::from("");
+        if savecakes {
+            cakedir = format!("{}/{}", &cbfdir, &outsubdir)
+        }
+        let avdir = format!("{}/{}", &cbfdir, &outsubdir);
+        let _ = create_dir(&avdir);
+        println!("collecting ponis");
+        let mf = MultiFile::build(&cbfdir, &ponidir, tthmin, tthmax, tthbins, chimin, chimax, chibins, pfactor, mask);
+        let t = timestamp.elapsed();
+        println!("time elapsed: {}",t.as_secs());
+        mf.average_cakes(4., &cakedir, &avdir, cakemaskfile);
+        println!("cakes averaged, total time {}", timestamp.elapsed().as_secs());
+    }
+}
